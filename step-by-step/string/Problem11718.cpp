9d68cf4e01d52df9afbf19dbb3ae819de1c8e55e
정가온 <gaon0967@naver.com>
2025-03-27 23:48:01 +0900

    Add Problem11718 solution

---
+++ step-by-step/string/Problem11718.cpp
@@ -0,0 +1,16 @@
+// 백준 11718번 문자열-그대로 출력하기
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	while (getline(cin, s)) {
+		cout << s << endl;
+	}
+	return 0;
+}
+
+
