afb34cc66a8b97d857eecfc15c7ac8317112691a
정가온 <gaon0967@naver.com>
2025-03-27 23:51:39 +0900

    Add Problem25083 solution

---
+++ step-by-step/Advanced-1/Problem25083.cpp
@@ -0,0 +1,16 @@
+// 백준 25083번 심화1-새싹
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	cout << "         ,r'\"7" << endl;
+	cout << "r`-_   ,'  ,/" << endl;
+	cout << " \\." << " \". L_r'" << endl;
+	cout << "   `~\\/" << endl;
+	cout << "      |" << endl;
+	cout << "      |" << endl;
+	return 0;
+}
