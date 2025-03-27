8c8964317bdd1275cc8bef2c1fac83ecec2145fa
정가온 <gaon0967@naver.com>
2025-03-27 23:36:33 +0900

    Add Problem2743 solution

---
+++ step-by-step/string/Problem2743.cpp
@@ -0,0 +1,16 @@
+// 백준 2743번 문자열-단어 길이 재기
+
+  GNU nano 8.3                    Problem2743.cpp                     Modified
+// 백준 2743번 문자열-단어 길이 재기
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	string S;
+	
+	cin >> S;
+	cout << S.length();
+	return 0;
+}
