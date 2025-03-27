cf26906582d7b1d1ab0f9954e2b05c28d3c1e385
정가온 <gaon0967@naver.com>
2025-03-27 22:48:55 +0900

    Add Problem25314 solution

---
+++ step-by-step/loop/Problem25314.cpp
@@ -0,0 +1,19 @@
+// 백준 25314번 반복문-코딩은 체육과목 입니다.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	string s = "long ";
+	for (int i = 1; i <= N / 4; i++) {
+		cout << s;
+	}
+	cout << "int";
+	return 0;
+}// 백준 25314번 반복문-코딩은 체육과목 입니다
+
+
+
+
