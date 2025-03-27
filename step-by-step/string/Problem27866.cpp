0edd7e5f685848c244569580f0ea8e5361f67b8b
정가온 <gaon0967@naver.com>
2025-03-27 23:34:53 +0900

    Add Problem27866 solution

---
+++ step-by-step/string/Problem27866.cpp
@@ -0,0 +1,15 @@
+// 백준 27866번 문자열-문자와 문자열
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	string S;
+	int i;
+
+	cin >> S >> i;
+
+	cout << S[i - 1] << endl;
+	return 0;
+}
