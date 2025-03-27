2d5efed755be5f0d37649bfcda9e404398d658d8
정가온 <gaon0967@naver.com>
2025-03-27 23:14:22 +0900

    Add Problem10951 solution

---
+++ step-by-step/loop/Problem10951.cpp
@@ -0,0 +1,14 @@
+// 백준 10951번 반복문-A+B(4)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int A, B;
+	
+	while (cin >> A >> B) {
+		cout << A + B << endl;
+	}
+	return 0;
+}
