deed64b615f75d61c961214c073842a665a02cb0
gaon0967 <gaon0967@naver.com>
2025-04-04 15:16:32 +0900

    Create Problem5086.cpp

---
+++ step-by-step/DMP-math/Problem5086.cpp
@@ -0,0 +1,36 @@
+// 백준 5086번 약수,배수와 소수-배수와 약수
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<int> v1, v2; // 값 저장
+	int num1, num2; // 추가할 값
+
+	while (true) {
+		cin >> num1 >> num2;
+		if (num1 == 0 & num2 == 0) break;
+		v1.push_back(num1);
+		v2.push_back(num2);
+	} // 벡터에 숫자 저장
+
+	for (int i = 0; i < v1.size(); i++) {
+		for (int j = 0; j < v2.size(); j++) {
+			if (v2[i] % v1[i] == 0) {
+				cout << "factor" << endl;
+				break;
+			}
+			else if (v1[i] % v2[i] == 0) {
+				cout << "multiple" << endl;
+				break;
+			}
+			else {
+				cout << "neither" << endl;
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
